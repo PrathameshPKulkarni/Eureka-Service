236817f0143a7f23c5c28f12f5987337cd2ccd33
Prathamesh Kulkarni <PKulkarni@texala.net>
2024-04-23 16:38:57 +0530

    initial project setup

---
+++ src/test/java/net/texala/Eureka/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.texala.Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
